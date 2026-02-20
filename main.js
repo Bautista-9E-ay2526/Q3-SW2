b1073337dbe01ecd0d394e79a194174720719935
Bautista-9E-ay2526 <bautista.nr@obmontessori.edu.ph>
2026-02-20 11:26:49 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//Function for Discount Button
+function buttonDiscount () {
+    let Budge = document.getElementById(budget).value;
+    let discval = Number(Budge * 0.2)
+
+    document.getElemetnById().InnerHTML = 
+        <div class="container"></div>
+}
